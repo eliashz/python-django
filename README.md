6935d2b4974e56e58ca1874155145490160decfe
eliashz <eliashz@gmail.com>
2023-04-10 22:39:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+virtual env: https://sourabhbajaj.com/mac-setup/Python/virtualenv.html
+`source venv/bin/activate`
+
+Base de Datos: https://docs.djangoproject.com/en/4.2/ref/databases/
---
+++ djangoproject/myapp/views.py
@@ -0,0 +1,8 @@
+from django.http import HttpResponse
+
+# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Holi!</h1>")
+
+def about(request):
+    return HttpResponse("About!")
